5e0056ff8a13dae4be6908b1e11cbe7d0f2f7104
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-07 09:18:44 +0300

    updated poem components

--- my-poem-app/src/App.js
+++ my-poem-app/src/App.js
@@ -1,40 +1,14 @@
 import './App.css';
-import AuthorContainer from './components/Authors';
-import PoemForm from './components/AddPoem';
+import NavBar from './components/NavBar';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return(
-    <div className="container-1">
-      <div className="box-1">
-      <h1>THIS IS THE MAN WITH THE BASS</h1>
-      <p>How do you make a dummy text in Visual Studio code?
-        Today's VS Code tip: Emmet lorem Just type lorem in 
-        #html to generate a paragraph of dummy text. Control 
-        how much text is generated with a number suffix, 
-        such as lorem10 to generate 10 words of dummy text
-        You can also combine lorem with other Emmet
-        How do you make a dummy text in Visual Studio code?
-        Today's VS Code tip: Emmet lorem Just type lorem in 
-        #html to generate a paragraph of dummy text. Control 
-        how much text is generated with a number suffix, 
-        such as lorem10 to generate 10 words of dummy text
-        You can also combine lorem with other Emmet
-      </p>
-      </div>
-      <div className="box-2">
-      <AuthorContainer/>
-      <PoemForm/>
-      <p>#html to generate a paragraph of dummy text. Control 
-        how much text is generated with a number suffix, 
-        such as lorem10 to generate 10 words of dummy text
-        You can also combine lorem with other Emmet
-      </p>
-      </div>
-      {/* <div className="box-3"></div>
-      <div className="box-4"></div>
-      <div className="box-5"></div> */}
-
-
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route></Route>
+      </Switch>
     </div>
   )
 }
