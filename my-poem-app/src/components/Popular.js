a08fbe789afe384c5939651c6880bbcda67ca620
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-13 18:35:16 +0300

    fetching data for the popular component

--- my-poem-app/src/components/Popular.js
+++ my-poem-app/src/components/Popular.js
@@ -1,7 +1,18 @@
+import React, { useEffect, useState } from "react";
+
 function Popular(){
+    const[favPoems, setFavPoems]= useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:9292/favourites")
+        .then((resp)=>resp.json())
+        .then((favPoems)=>{
+            setFavPoems(favPoems)
+        })
+    },[]);
     return (
         <div>
-            <h1>Here we will have our categories section</h1>
+            <h1>This list shows the top 10 poems with most likes</h1>
         </div>
     )
 }
